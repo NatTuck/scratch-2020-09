d2b2ac711a88b04e1deeaa795775ca196d84031b
Nat Tuck <nat@ferrus.net>
2020-11-02 15:26:54 -0500

    updates

--- 3650/p10a/01/queue.c
+++ 3650/p10a/01/queue.c
@@ -1,40 +1,23 @@
-//#include <pthread.h>
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdatomic.h>
 #include <assert.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdio.h>
 #include <sys/mman.h>
 
-#define QUEUE_SIZE 8 // power of two
-#define NN 1024      // textual replacement done by the preprocessor
-const int NNV = 1024; // variable that the compiler sees
+#define QUEUE_SIZE 8
+#define NN 1024
 
-typedef struct shq {
+typedef struct shq { // shared queue
     int queue[QUEUE_SIZE];
-    unsigned int qii; // get index
-    unsigned int qjj; // put index
+    unsigned int qii; // removal index
+    unsigned int qjj; // insertion index
     sem_t isem; // space
     sem_t osem; // items
-    // note, invariant: isem + osem = 8
-    // qii - qjj is between 0 and 8 ... modulo 2^32
 } shq;
-
-// Question: 
-//  - What happens when qii or qjj overflow?
-//  - Does this break anything?
-//
-// Simpler question - with 16-bit ints.
-//
-//  unsigned short ii = 65535;
-//  ii += 1;
-//  What's ii? ii == 0
-//
-//  65535 % 8 = 7
-//  (65535 + 1) % 8 = 0
-//  ((65535 + 1) % 65536) % 8 = 0
 
 shq* shared = 0;
 
@@ -60,10 +43,10 @@ queue_put(int xx)
     int rv;
     rv = sem_wait(&(shared->isem));
     assert(rv == 0);
-    
-    unsigned int jj = atomic_fetch_add(&(shared->qjj), 1);
-    shared->queue[jj % QUEUE_SIZE] = xx;
-    
+   
+    unsigned int ii = atomic_fetch_add(&(shared->qjj), 1);
+    shared->queue[ii % QUEUE_SIZE] = xx;
+
     rv = sem_post(&(shared->osem));
     assert(rv == 0);
 }
@@ -83,12 +66,13 @@ main(int _ac, char* _av[])
 
     shared = mmap(0, 1024*1024, PROT_READ|PROT_WRITE,
             MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    assert((long)shared != -1);
 
     shared->qii = 0;
     shared->qjj = 0;
 
-    sem_init(&(shared->isem), 1, QUEUE_SIZE);
-    sem_init(&(shared->osem), 1, 0);
+    sem_init(&(shared->isem), 1, QUEUE_SIZE); // free spaces
+    sem_init(&(shared->osem), 1, 0); // items in queue
 
     for (int ii = 0; ii < 2; ++ii) {
         kids[ii] = fork();
@@ -98,7 +82,7 @@ main(int _ac, char* _av[])
         }
     }
 
-    for (int ii = 0; ii < 2*NN; ++ii) {
+    for (int ii = 0; ii < 2 * NN; ++ii) {
         int yy = queue_get();
         printf("%d\n", yy);
         usleep(1000);
@@ -110,4 +94,11 @@ main(int _ac, char* _av[])
 
     return 0;
 }
+
+
+
+
+
+
+
 
